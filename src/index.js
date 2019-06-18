8ffd915b56123c1785f0ad919721173005332c41
Andrij <andrey.m.savchuk@gmail.com>
2019-06-18 17:20:25 +0300

    0001

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { products } from "./products";
-import "./styles.css";
 
 function Product(props) {
   const { items } = props;
